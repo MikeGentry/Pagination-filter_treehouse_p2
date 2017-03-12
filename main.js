a9edaa4112041317910f8c94c881e825e96d08ac
Mike Gentry <mgentry007@gmail.com>
2017-03-11 20:15:32 -0800

    Required Functionality Complete

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
+const pageList = document.querySelector('ul');
+const eachStudent = pageList.children;
+const buttonDiv = document.querySelector('.pagination');
+const buttonUl = buttonDiv.querySelector('ul');
+const studentsPerPage = 10;
 
+// Function to determine number of pages based on number of students
+function numberOfPages() {
+    let pages = Math.ceil(eachStudent.length / studentsPerPage);
+    return pages;
+}
 
-const eachStudent = document.querySelectorAll('li');
+// Loop to create page buttons based on number of required pages
+for (let i = 1; i <= numberOfPages(); i++) {
+    let pageli = document.createElement('li');
+    let pageLink = document.createElement('a');
+    pageLink.className = 'active';
+    pageLink.href = '#';
+    pageLink.textContent = i;
+    buttonUl.appendChild(pageli);
+    pageli.appendChild(pageLink);
+}
 
+// Function to automatically show first ten students when page loads
+function showFirstTen() {
+    for (let i = 0; i < eachStudent.length; i++) {
+        if (i < studentsPerPage) {
+            eachStudent[i].style.display = '';
+        } else {
+            eachStudent[i].style.display = 'none';
+        }
+    }
+}
 
+// Event listener to divide students between pages
+buttonDiv.addEventListener('click', (event) => {
+    let buttonNumber = parseInt(event.target.textContent);
+    let max = buttonNumber * 10; 
+    let min = max - 10;
+    for (let i = 0; i < eachStudent.length; i++) {
+        if (i >= min && i < max) {
+            eachStudent[i].style.display = '';
+        }  else {
+            eachStudent[i].style.display = 'none';
+        }
+    }    
+});
+
+// Function call to display first ten students on load
+showFirstTen();
